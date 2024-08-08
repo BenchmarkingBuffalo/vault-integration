58b35ff17f1acabd999e3059ce314e9b77cb501b
Ravi <rkamineni@ingenico.com>
2024-08-08 19:00:54 +0530

    fetching db creds from vault working

--- src/main/java/com/examples/vaultintegration/controller/EmployeeController.java
+++ src/main/java/com/examples/vaultintegration/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.examples.vaultintegration.controller;
 import com.examples.vaultintegration.dao.EmployeeRepository;
 import com.examples.vaultintegration.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +15,6 @@ public class EmployeeController {
     @Autowired
     EmployeeRepository employeeRepository;
 
-    //@Autowired
-    //DataSourceConfig dataSourceConfig;
-
     @GetMapping
     public ResponseEntity<Employee> getEmployee() {
         Employee employee = employeeRepository.getEmployee(1);
@@ -25,7 +23,6 @@ public class EmployeeController {
 
     @PostMapping
     public ResponseEntity saveEmployee(@RequestBody Employee emp) {
-        //System.out.println("dbconfig.user: " + dataSourceConfig.getUsername() +" "+ dataSourceConfig.getPassword());
         employeeRepository.saveEmployee(emp);
         System.out.println("employee saved");
         return new ResponseEntity(HttpStatus.CREATED);
